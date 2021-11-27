1ffb7bdf576bc6cbef20212de59a711582fd13a0
ecyontay01@monccnb.ca <you@example.com>
2021-11-27 13:42:14 -0400

    envoi du code

---
+++ include/Secrets.h
@@ -0,0 +1,26 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+#define WIFI_SECRET_SSID "237HomeTyga"
+#define WIFI_SECRET_PASS "Overdose2021"
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN  "XLcj8VPRPO5hWg45vJSE"
+#define SECRET_DEVICE_ID "e4123230-4c5d-11ec-9235-d318ce92df6e"
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+/*
+  Titre      : Titre du programme
+  Auteur     :https://wiki.dfrobot.com/Analog_Piezo_Disk_Vibration_Sensor__SKU_DFR0052_#target_4
+  Date       : 27/11/2021
+  Description: Une description du programme
+  Version    : 0.0.1
+*/
+
+
+
+#include <Arduino.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h"
+
+
+int GreenLED = 3;
+int RedLED = 2;
+
+bool  StatusFerme = true ;
+bool StatusOuvert = false ;
+
+
+
+const int analogPin = A1;
+
+
+
+void setup()
+{
+  Serial.begin(9600); //
+
+ wifiConnect();                  //Branchement au réseau WIFI
+  MQTTConnect();                  //Branchement au broker MQTT
+
+
+  pinMode(analogPin,INPUT); 
+   pinMode(GreenLED, OUTPUT);
+  pinMode(RedLED, OUTPUT);
+}
+void loop()
+{
+      int val;
+      val=analogRead(analogPin);//Connect the sensor to analog pin 0
+
+if(val < 50){
+    digitalWrite(RedLED, HIGH);
+    digitalWrite(GreenLED, LOW);
+     StatusFerme = true ;
+     StatusOuvert =  false ;
+
+    
+  }else if(val >= 50){  
+    digitalWrite(GreenLED, HIGH);
+    digitalWrite(RedLED, LOW);
+    StatusFerme =  false ;
+    StatusOuvert =  true ;
+
+    
+  }
+
+
+      Serial.print("La valeur de vibration obtenue ");
+      Serial.println(val,DEC);//
+
+      appendPayload("Vibration",val );  //Ajout de la donnée température au message MQTT
+  sendPayload();                                   //Envoie du message via le protocole MQTT
+
+  appendPayload("Statutferme", StatusFerme);
+ appendPayload("Statutouvert", StatusOuvert);
+
+      delay(1000);
+}
